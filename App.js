cf88b3e7f96c0796d30f55747f6728e0585a8b0c
stjanis <janis.stipnieks@novasol.com>
2016-04-19 16:46:43 +0200

    initial commit

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+// class component -- can have state
+class App extends React.Component {
+  render() {
+    return (
+      <h1>Hello world!</h1>
+    )
+  }
+}
+
+// stateless function Component -- will not have state
+// const App = () => <h1>Hello stateless</h1>
+
+export default App;

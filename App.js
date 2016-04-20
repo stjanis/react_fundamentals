9d2ca826d4c16b14c5c9084c1531b6e3d0a2f3f7
stjanis <janis.stipnieks@novasol.com>
2016-04-20 11:31:02 +0200

    6: owner - ownee relationship

--- App.js
+++ App.js
@@ -3,6 +3,9 @@ import React from 'react';
 // unlike props which are meant to be passed in component as static values or methods
 // state is a collection of values that is meant to be managed by component itself
 
+// when one component renders another component this is what reacts refers to as
+// owner - ownee relationship, where parent component is also called a composit component
+
 // class component -- can have state
 class App extends React.Component {
   // initialize state
@@ -10,9 +13,9 @@ class App extends React.Component {
     // super gives a context for "this" within our component
     super();
     this.state = {
-      txt: 'this is the state txt',
-      cat: 0
+      txt: ''
     }
+    this.update = this.update.bind(this)
   }
   // to manage state
   update(e) {
@@ -25,11 +28,24 @@ class App extends React.Component {
     // h1: accessing the state value
     return (
       <div>
-        <input type="text" onChange={ this.update.bind(this) } />
-        <h1>{ this.state.txt }</h1>
+        <Widget txt={ this.state.txt } update={ this.update } />
+        <Widget txt={ this.state.txt } update={ this.update } />
+        <Widget txt={ this.state.txt } update={ this.update } />
+        <Widget txt={ this.state.txt } update={ this.update } />
       </div>
     )
   }
+}
+
+// create a new component based on App component
+// this will be stateless component
+const Widget = (props) => {
+  return (
+    <div>
+      <input type="text" onChange={ props.update } />
+      <h1>{ props.txt }</h1>
+    </div>
+  )
 }
 
 export default App

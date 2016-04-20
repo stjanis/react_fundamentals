102b85423d88f5a25b016b62401e4086af0e6816
stjanis <janis.stipnieks@novasol.com>
2016-04-20 13:19:18 +0200

    7: using refs to access components

--- App.js
+++ App.js
@@ -1,51 +1,55 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 
-// unlike props which are meant to be passed in component as static values or methods
-// state is a collection of values that is meant to be managed by component itself
-
-// when one component renders another component this is what reacts refers to as
-// owner - ownee relationship, where parent component is also called a composit component
+// refs
+// refs are a way to reference an instance of a component from within react application
 
 // class component -- can have state
 class App extends React.Component {
-  // initialize state
   constructor() {
-    // super gives a context for "this" within our component
     super();
     this.state = {
-      txt: ''
+      red: 0,
+      green: 0,
+      blue: 0
     }
     this.update = this.update.bind(this)
   }
-  // to manage state
   update(e) {
-    //setState methods sets new state
-    this.setState({ txt: e.target.value });
+    this.setState({
+      red: ReactDOM.findDOMNode(this.refs.red).value,
+      green: ReactDOM.findDOMNode(this.refs.green).value,
+      blue: ReactDOM.findDOMNode(this.refs.blue).value
+    });
   }
   // in class component we will always have render method
   render() {
-    // input: triggering update on state
-    // h1: accessing the state value
     return (
       <div>
-        <Widget txt={ this.state.txt } update={ this.update } />
-        <Widget txt={ this.state.txt } update={ this.update } />
-        <Widget txt={ this.state.txt } update={ this.update } />
-        <Widget txt={ this.state.txt } update={ this.update } />
+        <Slider ref="red" update={ this.update } />
+        { this.state.red }
+        <br />
+        <Slider ref="green" update={ this.update } />
+        { this.state.green }
+        <br />
+        <Slider ref="blue" update={ this.update } />
+        { this.state.blue }
+        <br />
       </div>
     )
   }
 }
 
-// create a new component based on App component
-// this will be stateless component
-const Widget = (props) => {
-  return (
-    <div>
-      <input type="text" onChange={ props.update } />
-      <h1>{ props.txt }</h1>
-    </div>
-  )
+// !! refs will not work with stateless components
+class Slider extends React.Component {
+  render() {
+    return (
+        <input type="range"
+          min="0"
+          max="255"
+          onChange={ this.props.update } />
+    )
+  }
 }
 
 export default App
